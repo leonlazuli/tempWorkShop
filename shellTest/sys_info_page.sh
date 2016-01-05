37c467c9228ed958fb87575d3f0419d71e1fee1a
Leon Lazuli <leonlazuli@gmail.com>
2016-01-05 18:08:52 +0800

    some shell script

---
+++ shellTest/sys_info_page.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Leon's shell, output a system information
+
+echo "<HTML>"
+echo "    <HEAD>"
+echo "        <TITLE>Page Title</TITLE>"
+echo "    </HEAD>"
+echo "    <BODY>"
+echo "        Page body"
+echo "    </BODY>"
+echo "</HTML>"
